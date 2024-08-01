3375f8eebef13346e2b92fb1d7ffb30157f451ad
Moises <moiseswtf123@gmail.com>
2024-07-31 19:32:06 -0500

    subiendo proyecto

---
+++ src/assets/styles/GlobalStyle.jsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+// import "@fontsource/poppins";
+// import 'primereact/resources/themes/saga-blue/theme.css';
+// import 'primereact/resources/primereact.min.css';
+// import 'primeicons/primeicons.css';
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+        font-family: sans-serif;
+        /* font-family: "Poppins", sans-serif; */
+    }
+    h1,h2,h3,h4,h5,h6{
+    margin: 0;
+    padding: 0;
+}
+a{
+    color: inherit;
+    text-decoration:none;
+}
+`;
